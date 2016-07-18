83197933ff6c8f38885750f6081ef641eee25299
LukasKowalczyk <kowalczyk.lukas.1987@gmail.com>
2016-07-18 22:32:09 +0200

    fix

--- src/de/lapi/comparator/TextElementComparator.java
+++ src/de/lapi/comparator/TextElementComparator.java
@@ -4,10 +4,11 @@ import java.util.Comparator;
 
 import de.lapi.container.TextElement;
 
-public class TextElementComparator implements Comparator<TextElement> {
+public class TextElementComparator implements Comparator<Object> {
 
-	@Override
-	public int compare(TextElement e1, TextElement e2) {
+	public int compare(Object arg0, Object arg1) {
+		TextElement e1=(TextElement) arg0;
+		TextElement e2=(TextElement) arg1;
 		return e1.getId().compareTo(e2.getId());
 	}
 

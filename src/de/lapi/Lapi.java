f37296f6d8f08fbb063787c63df26bd2fd6cea62
LukasKowalczyk <kowalczyk.lukas.1987@gmail.com>
2016-07-21 14:55:03 +0200

    defaultlocation can be changed

--- src/de/lapi/Lapi.java
+++ src/de/lapi/Lapi.java
@@ -15,6 +15,8 @@ import de.lapi.container.LanguageText;
 import de.lapi.container.TextElement;
 
 public class Lapi {
+	private static Locale defaultLocation = Locale.getDefault();
+
 	private Map<String, List<TextElement>> textLanguages = new HashMap<String, List<TextElement>>();
 
 	private static String quellOrdner = null;
@@ -63,13 +65,12 @@ public class Lapi {
 
 	public void generateDefaultLanguageText() {
 		try {
-			Locale local = Locale.getDefault();
-			if (textLanguages.containsKey(local.getISO3Language())) {
+			if (textLanguages.containsKey(defaultLocation.getISO3Language())) {
 				return;
 			}
-			File file = loadFile(local);
+			File file = loadFile(defaultLocation);
 			LanguageText lt = JAXB.unmarshal(file, LanguageText.class);
-			textLanguages.put(local.getISO3Language(), lt.getTextElements());
+			textLanguages.put(defaultLocation.getISO3Language(), lt.getTextElements());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -80,39 +81,35 @@ public class Lapi {
 		return findText(id, out);
 	}
 
-	public String getText(Locale local, Class<?> mainClass,
-			Class<?> objectClass, String name) {
+	public String getText(Locale local, Class<?> mainClass, String name) {
 		List<TextElement> out = textLanguages.get(local.getISO3Language());
-		String id = generateKey(mainClass, objectClass, name);
+		String id = generateKey(mainClass, name);
 		return findText(id, out);
 	}
 
-	public String getText(Class<?> mainClass, Class<?> objectClass, String name) {
-		Locale local = Locale.getDefault();
-		List<TextElement> out = textLanguages.get(local.getISO3Language());
-		String id = generateKey(mainClass, objectClass, name);
+	public String getText(Class<?> mainClass, String name) {
+		List<TextElement> out = textLanguages.get(defaultLocation.getISO3Language());
+		String id = generateKey(mainClass, name);
 		return findText(id, out);
 	}
 
 	public String getText(String id) {
-		List<TextElement> out = textLanguages.get(Locale.getDefault()
+		List<TextElement> out = textLanguages.get(defaultLocation
 				.getISO3Language());
 		return findText(id, out);
 	}
 
 	private String findText(String id, List<TextElement> out) {
 		for (TextElement textElement : out) {
-			if(textElement.getId().equals(id)){
+			if (textElement.getId().equals(id)) {
 				return textElement.getText();
 			}
 		}
 		return "##Text not found##";
 	}
 
-	private String generateKey(Class<?> mainClass, Class<?> objectClass,
-			String extension) {
-		String out = mainClass.getSimpleName() + "."
-				+ objectClass.getSimpleName() + "." + extension;
+	private String generateKey(Class<?> mainClass, String extension) {
+		String out = mainClass.getSimpleName() + "." + extension;
 		return out;
 	}
 
@@ -131,5 +128,13 @@ public class Lapi {
 
 	public static void setQuellOrdner(String quellOrdner) {
 		Lapi.quellOrdner = quellOrdner;
+	}
+
+	public static Locale getDefaultLocation() {
+		return defaultLocation;
+	}
+
+	public static void setDefaultLocation(Locale defaultLocation) {
+		Lapi.defaultLocation = defaultLocation;
 	}
 }
